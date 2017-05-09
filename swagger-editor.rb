4fb51155c3d3e6e552e2f03657811e3b21b18c18
J$ <jmonette@homeaway.com>
2017-05-09 17:11:39 -0500

    test

---
+++ swagger-editor.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class SwaggerEditor < Formula
+  homepage 'http://jmoney8080.github.io/dotfiles'
+  url 'http://www.google.com'
+  sha256 'cf31291083030aa73cfaddd69fa530918a4c0020f54c376458a4f499422e2a69'
+  version '0.1'
+
+  head 'https://github.com/jmoney8080/homebrew-swagger.git'
+
+  def install
+    ohai 'empty'
+  end
+end
