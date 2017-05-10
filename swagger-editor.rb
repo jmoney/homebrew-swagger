7d6af4fa71a8cae46c656b1fdc400185e4bcff4b
J$ <jmonette@homeaway.com>
2017-05-10 09:57:38 -0500

    fix sha256

--- swagger-editor.rb
+++ swagger-editor.rb
@@ -3,7 +3,7 @@ require 'formula'
 class SwaggerEditor < Formula
   homepage 'http://jmoney8080.github.io/dotfiles'
   url 'https://github.com/jmoney8080/swagger-editor-plist/archive/v1.0.tar.gz'
-  sha256 '5bbf09385a478c95fbf55c345b6c6279489e8f4b'
+  sha256 '8022b0671fcf2c10f936e673099065ea6d25e6a020ae5fdd1dc2912e6179780b'
 
   def install
     ohai "#{prefix}/swagger"
